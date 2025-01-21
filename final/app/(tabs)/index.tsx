50e0a5c4f32c075865b84ce7c3a4e5c4324b72a3
Adrian Valenzuela <adrianpaulvalenzuela@gmail.com>
2025-01-21 23:19:58 +0800

    Button add

--- final/app/(tabs)/index.tsx
+++ final/app/(tabs)/index.tsx
@@ -24,7 +24,9 @@ export default function HomeScreen() {
       </ThemedView>
 
       <View>
-        <Image source={require("./../../assets/images/button.jpg")} />
+        <Link href={"/courses"}>
+          <Image source={require("./../../assets/images/button.jpg")} />
+        </Link>
       </View>
 
       <Text>
